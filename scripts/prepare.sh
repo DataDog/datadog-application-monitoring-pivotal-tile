11db21206f9735a0bf35df522a183d1678258fb8
NouemanKHAL <noueman.khal@gmail.com>
2022-08-19 12:50:46 +0200

    bump buildpack version

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,7 +4,7 @@ set -e
 
 RESOURCES_DIR="tile/resources"
 
-BUILDPACK_VERSION=${BUILDPACK_VERSION:-4.31.0}
+BUILDPACK_VERSION=${BUILDPACK_VERSION:-4.33.0}
 
 # download cloudfoundry buildpack zip
 curl -L "https://github.com/DataDog/datadog-cloudfoundry-buildpack/releases/download/$BUILDPACK_VERSION/datadog-cloudfoundry-buildpack.zip" -o $RESOURCES_DIR/datadog-cloudfoundry-buildpack.zip

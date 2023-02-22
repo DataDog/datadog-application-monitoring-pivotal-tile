31fc95ac00fd9bd148cddf26b5199af17197d353
NouemanKHAL <noueman.khalikine@datadoghq.com>
2023-02-22 15:57:43 +0100

    Update scripts/prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,7 +4,7 @@ set -e
 
 RESOURCES_DIR="tile/resources"
 
-BUILDPACK_VERSION=4.33.0
+BUILDPACK_VERSION=4.35.1
 
 # download cloudfoundry buildpack zip
 curl -L "https://github.com/DataDog/datadog-cloudfoundry-buildpack/releases/download/$BUILDPACK_VERSION/datadog-cloudfoundry-buildpack.zip" -o $RESOURCES_DIR/datadog-cloudfoundry-buildpack.zip

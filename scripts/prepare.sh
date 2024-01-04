3913891205e3cab1b8b5ec00631fba3f9ed94dc3
NouemanKHAL <noueman.khalikine@datadoghq.com>
2024-01-04 13:02:54 +0100

    Add `bpm-release` (#53)
    * update prepare script to download bpm-release
    
    * add bpm on the datadog-application-monitoring deployments

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -12,8 +12,12 @@ fi
 RESOURCES_DIR="tile/resources"
 
 BUILDPACK_VERSION=4.36.0
+BPM_VERSION=1.2.7
 
 BUILDPACK_NAME=datadog-cloudfoundry-buildpack-$BUILDPACK_VERSION.zip
 
 echo "download $BUILDPACK_NAME"
 curl -L "https://github.com/DataDog/datadog-cloudfoundry-buildpack/releases/download/$BUILDPACK_VERSION/$BUILDPACK_NAME" -o $RESOURCES_DIR/datadog-cloudfoundry-buildpack.zip
+
+echo "download bpm-release $BPM_VERSION"
+curl -L "https://bosh.io/d/github.com/cloudfoundry/bpm-release?v=$BPM_VERSION" -o $RESOURCES_DIR/bpm-release.tgz
